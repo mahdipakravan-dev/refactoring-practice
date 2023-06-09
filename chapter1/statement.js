4df6ab1f7397198b4511687aa24dc46d81b3e381
Mahdi <mahdipakravan79@gmail.com>
2023-06-09 17:05:22 +0330

    refactor : put play in statementData

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -1,9 +1,21 @@
 export function statement(invoice, plays) {
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({} , aPerformance);
+        result.play = playFor(result)
+        return result
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID]
+    }
+
     const statementData = {}
     statementData.customer = invoice.customer
-    statementData.performances = invoice.performances
+    statementData.performances = invoice.performances.map(enrichPerformance)
+    console.log(statementData.performances)
     return renderPlainText(statementData, plays)
 }
+
 
 function renderPlainText(data , plays) {
     //ChangeFunctionDeclaration
@@ -14,14 +26,10 @@ function renderPlainText(data , plays) {
                 minimumFractionDigits: 2
             }).format(aNumber / 100)
     }
-    //ReplaceTempWithQuery
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID]
-    }
     //extracting function
     function amountFor(aPerformance) {
         let result = 0
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy":
                 result = 40000
                 if (aPerformance.audience > 30) {
@@ -36,13 +44,13 @@ function renderPlainText(data , plays) {
                 result += 300 * aPerformance.audience
                 break
             default:
-                throw new Error(`unknown type: ${playFor(aPerformance).type}`)
+                throw new Error(`unknown type: ${aPerformance.play.type}`)
         }
         return result
     }
     function volumeCreditsFor(aPerformance) {
         let result = Math.max(aPerformance.audience - 30, 0)
-        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5)
+        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5)
 
         return result
     }
@@ -63,7 +71,7 @@ function renderPlainText(data , plays) {
 
     let result = `Statement for ${data.customer}\n`
     for (let perf of data.performances) {
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seat)\n`
+        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seat)\n`
     }
     result += `Amount owed is ${usd(totalAmount())}\n`
     result += `You earned ${totalVolumeCredits()} credits\n`
