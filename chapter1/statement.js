c5384e2f064b8022baddd8c67ff838c857a8c457
Mahdi <mahdipakravan79@gmail.com>
2023-06-15 13:35:43 +0330

    [refactor] : SplitPhase final (move createStatementData)

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -1,61 +1,9 @@
+import {createStatementData} from "./createStatementData";
+
 export function statement(invoice, plays) {
-    function enrichPerformance(aPerformance) {
-        const result = Object.assign({}, aPerformance);
-        result.play = playFor(result)
-        result.amount = amountFor(result)
-        result.volumeCredit = volumeCreditsFor(result)
-        return result
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID]
-    }
-
-    //extracting function
-    function amountFor(aPerformance) {
-        let result = 0
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30)
-                }
-                break
-            case "comedy":
-                result = 30000
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20)
-                }
-                result += 300 * aPerformance.audience
-                break
-            default:
-                throw new Error(`unknown type: ${aPerformance.play.type}`)
-        }
-        return result
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = Math.max(aPerformance.audience - 30, 0)
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5)
-
-        return result
-    }
-
-    const statementData = {}
-    statementData.customer = invoice.customer
-    statementData.performances = invoice.performances.map(enrichPerformance)
-    console.log(statementData.performances)
-    return renderPlainText(statementData, plays)
+    return renderPlainText(createStatementData(invoice,plays))
 }
 
-//ChangeFunctionDeclaration
-function usd(aNumber) {
-    return new Intl.NumberFormat("en-US",
-        {
-            style: "currency", currency: "USD",
-            minimumFractionDigits: 2
-        }).format(aNumber / 100)
-}
 
 
 function renderPlainText(data) {
@@ -76,4 +24,12 @@ function renderPlainText(data) {
     result += `Amount owed is ${usd(totalAmount())}\n`
     result += `You earned ${totalVolumeCredits()} credits\n`
     return result
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US",
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber / 100)
 }
