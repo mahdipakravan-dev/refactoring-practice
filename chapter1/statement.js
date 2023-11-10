1c2bd203caa1f7bd8ab699f5a278b48a790d6f1d
mahdipakravan-dev <mahdipakravan79@gmail.com>
2023-11-10 23:17:36 +0330

    revert to initial

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -1,35 +1,41 @@
-import {createStatementData} from "./createStatementData";
-
 export function statement(invoice, plays) {
-    return renderPlainText(createStatementData(invoice,plays))
-}
-
-
-
-function renderPlainText(data) {
-    function totalVolumeCredits() {
-        return data.performances.reduce(
-            (total, aPerformance) => total + aPerformance.volumeCredit, 0)
-    }
-
-    function totalAmount() {
-        return data.performances.reduce(
-            (amount, aPerformance) => amount + aPerformance.amount, 0)
-    }
-
-    let result = `Statement for ${data.customer}\n`
-    for (let perf of data.performances) {
-        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seat)\n`
-    }
-    result += `Amount owed is ${usd(totalAmount())}\n`
-    result += `You earned ${totalVolumeCredits()} credits\n`
-    return result
-}
-
-function usd(aNumber) {
-    return new Intl.NumberFormat("en-US",
+    let totalAmount = 0
+    let volumeCredits = 0
+    let result = `Statement for ${invoice.customer}\n`
+    const format = new Intl.NumberFormat("en-US",
         {
             style: "currency", currency: "USD",
             minimumFractionDigits: 2
-        }).format(aNumber / 100)
+        }).format
+    for (let perf of invoice.performances) {
+        const play = plays[perf.playID]
+        let thisAmount = 0
+        switch (play.type) {
+            case "tragedy":
+                thisAmount = 40000
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30)
+                }
+                break
+            case "comedy":
+                thisAmount = 30000
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20)
+                }
+                thisAmount += 300 * perf.audience
+                break
+            default:
+                throw new Error(`unknown type: ${play.type}`)
+        }
+        // add volume credits
+        volumeCredits += Math.max(perf.audience - 30, 0)
+        // add extra credit for every ten comedy attendees
+        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5)
+        // print line for this order
+        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seat)\n`
+        totalAmount += thisAmount
+    }
+    result += `Amount owed is ${format(totalAmount / 100)}\n`
+    result += `You earned ${volumeCredits} credits\n`
+    return result
 }
